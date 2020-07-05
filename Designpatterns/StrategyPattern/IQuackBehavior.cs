38b464e24273ca0ddbba0a4c74e6bf2f088b451a
Vincent A <vinca@microsoft.com>
2020-07-05 21:22:51 +0530

    Added Strategy Design pattern

---
+++ Designpatterns/StrategyPattern/IQuackBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern
+{
+    public interface IQuackBehavior
+    {
+        public void Quack();
+    }
+
+    public class QuackQuack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Quack! Quack!");
+        }
+    }
+
+    public class MuteQuack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Cannot Quack da!");
+        }
+    }
+}
---
+++ Designpatterns/StrategyPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck();
+            mallardDuck.Display();
+            mallardDuck.PerformFly();
+            mallardDuck.PerformQuack();
+
+            Console.WriteLine("*******************");
+            Duck rubberDuck = new RubberDuck();
+            rubberDuck.Display();
+            rubberDuck.PerformFly();
+            rubberDuck.PerformQuack();
+
+            Console.WriteLine("*******************");
+            Duck modelDuck = new MallardDuck();
+            modelDuck.Display();
+            modelDuck.PerformFly();
+            Console.WriteLine("--- Converting behabiour using set method ---");
+            modelDuck.SetFlyBehavior(new FlyNoWay());
+            modelDuck.PerformFly();
+        }
+    }
+}
