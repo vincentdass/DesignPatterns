9032ffa7804f2ccd7d0294d3530c0fa7115b06f7
Vincent A <vinca@microsoft.com>
2020-07-25 17:36:10 +0530

    learnt decorator pattern

---
+++ Designpatterns/DecoratorPattern/AddonDecorator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public abstract class AddonDecorator : Beverage
+    {
+        internal Beverage beverage;
+        public override abstract double Cost();
+
+        public AddonDecorator(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+    }
+
+    public class Caramel : AddonDecorator
+    {
+        
+        public Caramel(Beverage beverage) : base(beverage)
+        {
+            Console.WriteLine("Added Caramel Addon");
+        }
+
+        public override double Cost()
+        {
+            Console.WriteLine("##DEBUG - In Caramel Cost()");
+            return this.beverage.Cost() + 1.0;
+        }
+    }
+
+    public class Chocolate : AddonDecorator
+    {
+        public Chocolate(Beverage beverage) : base(beverage)
+        {
+            Console.WriteLine("Added Chocolate Addon");
+        }
+
+        public override double Cost()
+        {
+            Console.WriteLine("##DEBUG - In Chocolate Cost()");
+            return this.beverage.Cost() + 2.0;
+        }
+    }
+}
---
+++ Designpatterns/DecoratorPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage b;
+            Console.Write("Select 1 for Espress / 2 for Decaf. Your Choice: ");
+            int t = Int32.Parse(Console.ReadLine());
+            if(t == 1)
+            {
+                b = new Espresso();
+            }
+            else
+            {
+                b = new DeCaf();
+            }
+            while(true)
+            {
+                Console.Write("Select 1 for Caramel / 2 for Chocolate / 3 for exit. Your choice: ");
+                int a = Int32.Parse(Console.ReadLine()); 
+                if(a == 3)
+                {
+                    break;
+                }
+                else if(a == 1)
+                {
+                    b = new Caramel(b);
+                }
+                else if(a == 2)
+                {
+                    b = new Chocolate(b);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option! select again");
+                }
+            }
+
+            Console.WriteLine($"Total Cost: {b.Cost()}");
+            Console.ReadLine();
+        }
+    }
+}
